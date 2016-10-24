b43a86922d43083a247f9978c071ad93a73ae042
植木 祐介 <uekiyusuke@ueki-no-MacBook-Pro-5.local>
2016-10-24 18:16:41 +0900

    Edited js and css file

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-$("h2").click (
-  function(){
-    $("img").each(function(i){
-    if($(this).hasclass("active")){
-    $(this).stop(true).animate({
-    "margin-left":(i*300+"px"),
-    "margin-top":(i*50+"px")
-    },1000);
-  }
-  else{
-    $(this).stop(true).animate({
-      "margin-left":(i*0+"px"),
-      "margin-top":(i*0+"px")
-    },1000);
-  });
+$(function(){
+    
 });
