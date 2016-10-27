8442599d2ea69c66fbedf7ac889fed0b0cedaae7
植木 祐介 <uekiyusuke@ueki-no-MacBook-Pro-5.local>
2016-10-27 16:34:04 +0900

    Add jQuery plugin and edit firstview

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 $(function(){
     // UAがスマホならデフォルト表示
-    
+    $(window).scroll(function(){
+        var vision_h = $(".vision_content").offset().top;
+        var business_h = $(".business_column").offset().top;
+        var about_h = $(".about dl").offset().top;
+    });
+
+    $('.firstview_slide').panelGallery({
+		viewDuration: 3000,
+		transitionDuration: 500,
+		boxSize: 45,
+		boxFadeDuration: 500,
+		boxTransitionDuration: 50,
+		FX: new Array('boxSouthEast')
+	});
 });
